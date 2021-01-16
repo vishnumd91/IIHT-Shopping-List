6a8a7eeda99cc042a42e13fbeb6e6f882d0a8d3d
Vishnu MD <vishnudivakaran1@gmail.com>
2021-01-16 13:32:59 +0530

    Local Environment Setup done (#10)
    * Local Environment Setup done
    
    * automation test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 const cors = require('cors');
@@ -9,8 +13,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const itemRoutes = require('./routes/items');
-
-const CONNECTION_URL = 'mongodb+srv://vishnu:vishnu@cluster0.uo9sf.mongodb.net/shopping-list?retryWrites=true&w=majority';
 
 app.use(cors());
 
@@ -20,11 +22,10 @@ app.use('/items', itemRoutes);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     mongoose.connection.once('open', () => {
         console.log('MongoDB Connected Succesfully');
     })
 )
 
-// app.get('/', (req,res) => res.send('<h1>Hello Guys</h1>'));
 
