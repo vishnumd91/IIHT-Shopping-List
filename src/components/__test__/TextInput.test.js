3063010080d9d734108517a8ec397b54164aa54d
vishnumd91 <vishnudivakaran1@gmail.com>
2021-01-16 15:18:45 +0530

    Initial unit test setup for run the app without crashing using jest

---
+++ src/components/__test__/TextInput.test.js
@@ -0,0 +1,11 @@
+import { cleanup } from "@testing-library/react";
+import React from "react";
+import ReactDOM from 'react-dom';
+import TextInput from '../TextInput';
+
+afterEach(cleanup);
+
+it('TextInput renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<TextInput></TextInput>, div);
+} )
