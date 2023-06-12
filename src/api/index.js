90ce74630a7b534d3af9924a2a2c3dea0766f639
Vishnu MD <vishnudivakaran1@gmail.com>
2023-06-13 01:35:05 +0530

    changing environment based on cra utility rules

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
