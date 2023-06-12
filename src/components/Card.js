dd74d38b06120e7d6e5b35619f169f2a192ad104
Vishnu MD <vishnudivakaran1@gmail.com>
2023-06-13 01:05:38 +0530

    Revert "adding new tenant into react app"
    This reverts commit b9dbdd38fee0fddc6bf734326627ca4d2cbb6eae.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,99 +1,95 @@
-import React, { useState, useEffect } from "react";
-import "./Card.css";
-import TextInput from "./TextInput";
+import React, { useState, useEffect } from 'react';
+import './Card.css';
+import TextInput from './TextInput';
 
 const Card = (props) => {
-  const message = "You have added no items yet!";
 
-  const baseUrl = "https://node-api-iiht-shopping-list.onrender.com/items";
+    const message = 'You have added no items yet!';
 
-  const { title } = props;
+    const baseUrl = 'https://nodejs-api-shopping-list.herokuapp.com/items';
 
-  const [item, setItem] = useState("");
+    const { title } = props;
 
-  const [list, setList] = useState([]);
+    const [item, setItem] = useState('')
 
-  useEffect(() => {
-    fetchItems();
-  }, []);
+    const [list, setList] = useState([])
 
-  async function fetchItems() {
-    const getItems = await fetch(`${baseUrl}/getItems`);
-    if (getItems.ok) {
-      const response = await getItems.json();
-      setList(response);
-    } else {
-      return document.write(`${getItems.status} : ${getItems.statusText}`);
+    useEffect(() => {
+        fetchItems();
+        }, [])
+
+    async function fetchItems() {
+        const getItems = await fetch(`${baseUrl}/getItems`);
+        if (getItems.ok) {
+            const response = await getItems.json();
+            setList(response);    
+        }
+        else {
+            return document.write(`${getItems.status} : ${getItems.statusText}`);
+        }
     }
-  }
 
-  const handleInputChange = (event) => {
-    setItem(event.target.value);
-  };
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    fetch(`${baseUrl}/postItems`, {
-      method: "POST",
-      body: JSON.stringify({
-        itemName: item,
-      }),
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((itemResponse) => setList(list.concat(itemResponse)));
-    setItem("");
-  };
-
-  const handleDeleteClick = (id) => {
-    if (window.confirm("Are you sure about deleting this item?")) {
-      fetch(`${baseUrl}/deleteItems/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then(() => fetchItems());
+    const handleInputChange = (event) => {
+        setItem(event.target.value)
     }
-    return fetchItems();
-  };
 
-  return (
-    <div className="cardStyle">
-      <div className="card">
-        <div className="card-header">{title}</div>
+    const handleClick = (event) => {
+        event.preventDefault();
+        fetch(`${baseUrl}/postItems`, {
+            method: 'POST',
+            body: JSON.stringify({
+                itemName: item,
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(itemResponse => setList(list.concat(itemResponse)));
+        setItem('')
+    }
 
-        {list.map((data) => {
-          const { _id, itemName } = data;
-          return (
-            <ul className="list-group list-group-flush">
-              <li key={_id} className="list-group-item">
-                {itemName}
-                <span className="trash">
-                  <i
-                    onClick={() => handleDeleteClick(_id)}
-                    className="fas fa-trash-alt"
-                  ></i>
-                </span>
-              </li>
-            </ul>
-          );
-        })}
+    const handleDeleteClick = (id) => {
+        if (window.confirm('Are you sure about deleting this item?')) {
+            fetch(`${baseUrl}/deleteItems/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(res=> res.json())
+            .then(() => fetchItems());    
+        }
+        return fetchItems();
+    }
 
-        {!list.length ? <p style={{ marginLeft: "35px" }}>{message}</p> : null}
-        <div className="card-header">
-          <TextInput
-            onChange={handleInputChange}
-            onClick={handleClick}
-            item={item}
-          ></TextInput>
+    return (
+        <div className = 'cardStyle'>
+            <div className="card">
+                <div className="card-header">
+                    {title}
+                </div>
+
+            {list.map((data) => {
+                const { _id, itemName } = data;
+                return(
+                    <ul className="list-group list-group-flush">
+                        <li key={_id} className="list-group-item">
+                        {itemName}
+                        <span className='trash'>
+                        <i onClick={() => handleDeleteClick(_id)} className="fas fa-trash-alt"></i>
+                        </span>
+                    </li>
+                    </ul>
+                );})}
+
+            {!list.length ? <p style={{marginLeft: '35px'}}>{message}</p> : null}
+            <div className="card-header">
+                <TextInput onChange={handleInputChange} onClick={handleClick} item={item}></TextInput>
+            </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Card;
+export default Card
